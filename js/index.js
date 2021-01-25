9caca7f66cffe54f152c6f2750fb39c931d30f07
007Rohan <72097646+007Rohan@users.noreply.github.com>
2021-01-25 21:13:17 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,39 @@
+// testimonils Slider
+
+$('#testimonils').owlCarousel({
+    loop: true,
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    margin: 10,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+})
+
+// Clients Slider
+$('#clients').owlCarousel({
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    margin: 10,
+    dots:true,
+    responsive:{
+        0:{
+            items:5
+        }
+    }
+})
+// Team Slider
+$('#team').owlCarousel({
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    margin: 10,
+    dots:true,
+    responsive:{
+        0:{
+            items:4
+        }
+    }
+})
